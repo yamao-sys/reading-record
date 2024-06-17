38f5812ba9ea13e0330ae0bc77d35d2aae2d1b82
yamao-sys <yamaopanku@gmail.com>
2024-06-17 09:47:09 +0900

    著者の入力値の型エラー: デフォルトを空文字にする

--- frontend/src/app/reading_records/_components/organisms/ReadingRecordEdit/index.tsx
+++ frontend/src/app/reading_records/_components/organisms/ReadingRecordEdit/index.tsx
@@ -75,7 +75,7 @@ export default function ReadingRecordEdit({ readingRecord }: ReadingRecordEditTy
           <input
             id='title'
             className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            value={inputReadingRecord.author}
+            value={inputReadingRecord.author ?? ''}
             onChange={(e) => updateInputReadingRecord({ author: e.target.value })}
           />
         </div>
